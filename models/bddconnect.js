cc99900adcf9e97e29f8a54621339f4ec97663cf
Juliette Rocher <rocher.juliette@hotmail.fr>
2020-07-09 11:11:23 +0200

    first commit

---
+++ models/bddconnect.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose')
+
+var options = {
+    connectTimeoutMS: 5000,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}
+
+mongoose.connect('mongodb+srv://admin:Tua4boas@cluster0.ltgrz.mongodb.net/TICKETAC?retryWrites=true&w=majority',
+   options,
+   function(err) {
+    if (err) {
+      console.log(`error, failed to connect to the database because --> ${err}`);
+    } else {
+      console.info('*** Database Ticketac connection : Success ***');
+    }
+   }
+);
+
+module.exports = mongoose
+
+
